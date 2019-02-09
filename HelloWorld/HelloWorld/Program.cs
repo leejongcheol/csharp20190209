38e5ffe0dab0ba23500ad5d8b1ad8e397c3c4c46
종철 이 <mylife68@nate.com>
2019-02-09 12:45:52 +0900

    C# HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld{
+    class A { }
+    class Program    {
+        int i;  //멤버, 필드, 인스턴스변수
+        static int j; //멤버, 변수, 클래스변수
+        static void Main(string[] args)  //멤버, 메소드
+        {
+            string name = "홍길동";
+            int k = 99;
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Hello {0}{1}", name, k);
+        }
+    }
+}
