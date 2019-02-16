443a5ab1ec6e75d0c7f5c77f2860e80879b2afa7
종철 이 <mylife68@nate.com>
2019-02-16 15:39:57 +0900

    Delegate Test

---
+++ DelegateTest2/DelegateTest2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ConsoleApplication11{
+    class Program    {
+        delegate int Calc(int i, int j);
+        static void Main(string[] args)        {
+            Calc c1 = (i, j) => i + j;
+            Console.WriteLine("3+4={0}", c1(3, 4));
+        }
+    }
+}
---
+++ DeligateTest/DeligateTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+delegate int SumDeli(int i, int j);
+class DeliTest
+{
+    static void Main()
+    {
+        S(Sum1); S(Sum2);
+    }
+    //아래 메소드는 실제 할일(콜백)이 컴파일 타임에 결정되는 것이 아니라 런타임중에
+    //인자로 넘어오는 델리게이트를 통해 결정된다. 이런 콜백의 구현은 델리게이트로
+    static void S(SumDeli s)
+    {
+        Console.WriteLine(s(1, 2));
+    }
+    static int Sum1(int i, int j)
+    {
+        return i + j;
+    }
+    static int Sum2(int i, int j)
+    {
+        return i + j + 100;
+    }
+}
