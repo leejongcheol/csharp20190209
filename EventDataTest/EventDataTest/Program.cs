e60c1b36dfe666840cab82982399e9bf55c6abe0
종철 이 <mylife68@nate.com>
2019-02-16 18:22:31 +0900

    Func Test

---
+++ EventDataTest/EventDataTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace EventTest {
+    class EventPublsher {
+        public event Action<int> ClapEvent;
+        public void Clap(int num) {
+            if (num % 2 == 0) ClapEvent(num);
+        }
+    }
+    class EventSub
+   {
+        public static void Main()
+        {
+            EventPublsher p = new EventPublsher();
+            p.ClapEvent += new Action<int>(Caller);
+
+            int[] iArr = Array.ConvertAll(
+                        (Console.ReadLine()).Split(','),
+                        i => int.Parse(i));
+
+            foreach (int i in iArr)
+            {
+                p.Clap(i);
+            }
+        }
+        static void Caller(int num)
+        {
+            Console.WriteLine("{0} : 짝~", num);
+        }
+    }
+}
---
+++ LambdaTest/LambdaTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApplication11{
+    class Program    {
+        static void Main(string[] args)        {
+            Func<int, int,  int> c3 = (a, b) => a + b;
+            Console.WriteLine("3+4={0}", c3(3, 4));
+        }
+        static int MySum(int i, int j)        {
+            return i + j;
+        }
+    }
+}
