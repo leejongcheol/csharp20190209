54ab32ac39f3dbb900f8693a8a6ad7f22eccdcac
종철 이 <mylife68@nate.com>
2019-02-16 14:21:27 +0900

    Action Delegate Test Sample

---
+++ ConditionalAttrTest/ConditionalAttrTest/Program.cs
@@ -0,0 +1,11 @@
+﻿ // 이 부분을 지운 후 실행 해 보자
+using System;
+using System.Diagnostics;
+class Test
+{
+    //Conditional Attribute인 경우 지정된 위치지정 파라미터가 정의 되어
+    //있을 때 실행이 되는 것이다.
+    [Conditional("SOUNDCARD")]
+    static void print() { Console.WriteLine("도레미..."); }
+    static void Main()     {        print();    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class Delegate1{
+    public static void Main(string[] args)    {
+        Action<int, int> myMethod =Sum;
+        myMethod.Invoke(10, 30);
+        myMethod(10, 30);        
+    }
+    static void Sum(int i, int j)
+    {
+        Console.WriteLine(i + j);
+    }
+}
