ecb491f4662566c90f148539747f26c523f976af
종철 이 <mylife68@nate.com>
2019-02-17 11:58:18 +0900

    Thread Test

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+public class ThreadTest{
+    public void FirstWork()    {
+        for (int i = 0; i < 100; i++)      {
+            Thread.Sleep(1);    Console.Write("F{0} ", i);
+        }
+    }
+    public void SecondWork()    {
+        for (int i = 0; i < 100; i++)        {
+            Thread.Sleep(1);    Console.Write("S{0} ", i);
+        }
+    }
+    public static void Main()    {
+        ThreadTest t = new ThreadTest();
+        //Thread는 생성자에 ThreadStart형 Delegate를 인자로 받는다.
+        Thread first = new Thread(t.FirstWork);
+        Thread second = new Thread(new ThreadStart(t.SecondWork));
+        first.Priority = ThreadPriority.Lowest;
+        second.Priority = ThreadPriority.Highest;
+        first.Start(); second.Start();
+    }
+}
