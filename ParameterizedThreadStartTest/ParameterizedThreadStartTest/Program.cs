a49ddec80e50bea282585cb9f22a9f3a45182db4
종철 이 <mylife68@nate.com>
2019-02-17 13:55:01 +0900

    Thread Lock Test

--- ParameterizedThreadStartTest/ParameterizedThreadStartTest/Program.cs
+++ ParameterizedThreadStartTest/ParameterizedThreadStartTest/Program.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
-namespace ParameteriedThreadStartTest
-{
-    class Program
-    {
-        static int mysum = 0;
-        static void Sum(object n)
-        {
-            int sum = 0;
-            int[] number = (int[])n;
-            for (int i = number[0]; i <= number[1]; i++)
-            {
-                sum += i;
+public class ThreadTest3 { 
+    public string lockString = "Hello";
+    private object obj = new object();
+    public void Print(string rank) {        
+        lock (obj)   {
+            for (int i = 0; i < 5; i++)    {
+                for (int j = 0; j < 5; j++)   {
+                    Thread.Sleep(100);
+                    Console.Write(",");
+                }
+                Console.WriteLine("{0}{1} ", rank, lockString);
             }
-            mysum += sum;
-        }
-        static void Main(string[] args)
-        {
-            Thread t1 = new Thread(new ParameterizedThreadStart(Sum));
-            Thread t2 = new Thread(new ParameterizedThreadStart(Sum));
-            Thread t3 = new Thread(new ParameterizedThreadStart(Sum));
-            Thread t4 = new Thread(new ParameterizedThreadStart(Sum));
-            Thread t5 = new Thread(new ParameterizedThreadStart(Sum));
-
-            t1.Start(new int[] { 1, 10 }); t2.Start(new int[] { 11, 20 });
-            t3.Start(new int[] { 21, 30 }); t4.Start(new int[] { 31, 40 });
-            t5.Start(new int[] { 41, 50 });
-
-            t1.Join(); t2.Join(); t3.Join(); t4.Join(); t5.Join();
-            Console.Write("1부터50까지의 합은{0}::", mysum);
         }
     }
-}
+    public void FirstWork() { Print("F"); }
+    public void SecondWork() { Print("S"); }
+}
+class TestMain
+{
+    public static void Main()
+    {
+        ThreadTest3 t = new ThreadTest3();
+        Thread first = new Thread(new ThreadStart(t.FirstWork));
+        Thread second = new Thread(new ThreadStart(t.SecondWork));
+        first.Start();
+        second.Start();
+    }
+}
